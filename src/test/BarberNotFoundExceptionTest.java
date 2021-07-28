79ff624967912026530eb8ee0872189acfcdd3aa
GUlshan <egmox2@gmail.com>
2021-07-29 05:00:21 +0530

    working on the tests

---
+++ src/test/BarberNotFoundExceptionTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+class BarberShop {
+
+	@Before
+	void testPredefineCustomers() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test() {
+		
+	}
+
+}
